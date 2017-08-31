ac0ce963476985607a5c5c0d783e9d59e4cef1eb
Sangram Haladkar <sangram.haladkar@mobifilia.com>
2017-08-31 10:52:33 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { OneSignal } from '@ionic-native/onesignal';
+import { AlertController } from 'ionic-angular';
+
+
+
 
 @Component({
   templateUrl: 'app.html'
@@ -14,9 +19,9 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private _OneSignal: OneSignal, public alertCtrl: AlertController) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -33,8 +38,40 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      //  OneSignal Setup .
+
+      this._OneSignal.startInit("1de94dd4-73ad-4817-b9b8-b5792c2f9c0e", "ionic-awesome-d0f40");
+      this._OneSignal.inFocusDisplaying(this._OneSignal.OSInFocusDisplayOption.Notification);
+      this._OneSignal.setSubscription(true);
+      this._OneSignal.handleNotificationReceived().subscribe(() => {
+        // handle received here how you wish.
+        this.showAlert("Zira", "Push Notification Received");
+      });
+      this._OneSignal.handleNotificationOpened().subscribe(() => {
+        // handle opened here how you wish.
+
+      });
+      this._OneSignal.endInit();
+
     });
   }
+
+
+  showAlert(subject, message) {
+    let alert = this.alertCtrl.create({
+      title: subject,
+      subTitle: message,
+      buttons: [{
+        text: 'Ok',
+        handler: () => {
+          console.log("Okay Clicked");
+        }
+      }]
+    });
+    alert.present();
+  }
+
 
   openPage(page) {
     // Reset the content nav to have just this page
